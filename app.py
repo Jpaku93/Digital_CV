3f2ee82ce913763771d59a04ddd3cf3da3218362
Jpaku93 <j.r.paku@hotmail.co.nz>
2022-12-19 13:53:34 +1300

    Update app.py

--- app.py
+++ app.py
@@ -79,18 +79,18 @@ st.write("""
 st.write("---")
 st.subheader("Experience")
 st.write("**💼 Data Science**, **Ai Engineer** | **Internship** | **Datacom** ")
-st.write(" ➤ Auckland, New Zealand (July 2022 - Nov 2022) - Complete")
+st.write("Auckland, New Zealand (July 2022 - Nov 2022) - Complete")
 st.write("""
-    - • Spatial analytics project to analyze people's foot traffic in office
-    - • Built a WIFI probing device and Fisheye lens camera person detection with Raspberry PI to collect data
-    - • Built Azure data pipeline - process, visualize on PowerBI
-    - • Selected as project Manager in a team of 4 students
+    - ➤ Spatial analytics project to analyze people's foot traffic in office
+    - ➤ Built a WIFI probing device and Fisheye lens camera person detection with Raspberry PI to collect data
+    - ➤ Built Azure data pipeline - process, visualize on PowerBI
+    - ➤ Selected as project Manager in a team of 4 students
          """)
 st.write("#")
 st.write("**💼 Research Assistant**, | **Media Design School** ")
-st.write(" ➤ Auckland, New Zealand (Nov 2022 - Feb 2022) - Present")
+st.write("Auckland, New Zealand (Nov 2022 - Feb 2022) - Present")
 st.write("""
-    - • Investigating data augmentation as a method to train visual recognition for indigenous data
+    - ➤ Investigating data augmentation as a method to train visual recognition for indigenous data
         """)
 
 # ------ TECHNICAL SKILLS ------
